8ae72165180821737c71abc7580cd0c6b4f517c2
flopp999 <21694965+flopp999@users.noreply.github.com>
2025-06-25 10:20:08 +0200

    Update Growatt.js

--- Growatt.js
+++ Growatt.js
@@ -4,7 +4,7 @@
 // License: Personal use only. See LICENSE for details.
 // This script was created by Flopp999
 // Support me with a coffee https://www.buymeacoffee.com/flopp999 
-let version = 0.24
+let version = 0.25
 let token;
 let deviceSn;
 let widget;
